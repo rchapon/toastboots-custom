ae293acb60e69db7694baf41673a78ae0681a85a
Rafael Rivas <ricardochapon@gmail.com>
2019-11-04 17:39:41 -0500

    Agrega mensajes en spanish and english

---
+++ demo/js/toastboots-custom/toastboots-custom-errors_es.js
@@ -0,0 +1,17 @@
+var msgs = {
+    "1":{"prop":"site      ","msg" :"No corresponde a ningun elemento del DOM"},
+    "2":{"prop":"titleBg   ","msg" :"No es una clase bootstrap"},
+    "3":{"prop":"titleTc   ","msg" :"No es una clase bootstrap"},
+    "4":{"prop":"subtitleTc","msg" :"No es una clase bootstrap"},
+    "5":{"prop":"timesTc   ","msg" :"No es una clase bootstrap"},
+    "6":{"prop":"contentTc ","msg" :"No es una clase bootstrap"},
+    "7":{"prop":"contentBg ","msg" :"No es una clase bootstrap"},
+    "8":{"prop":"animation ","msg" :"No fue definida correctamente"},
+    "9":{"prop":"autohide  ","msg" :"No fue definida correctamente"},
+   "10":{"prop":"delay     ","msg" :"No fue definida correctamente"},
+   "11":{"prop":"width     ","msg" :"No fue definida correctamente"},
+   "12":{"prop":"type      ","msg" :"No corresponde a ninguna combinacion preestablecida"},
+   "13":{"prop":"posy      ","msg" :"No fue definida correctamente"},
+   "14":{"prop":"posx      ","msg" :"No fue definida correctamente"}
+},
+head = "ERROR(ES)";
